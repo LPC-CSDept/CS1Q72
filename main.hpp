9e77883f08d5e0496a99105949bf35087559fd2c
Kyu Lee <kwooler@gmail.com>
2023-04-14 19:13:31 -0700

    Q72

--- main.hpp
+++ main.hpp
@@ -9,11 +9,5 @@ void printtriangle(int num[][SIZE])
 {
     ofstream ofs;
 
-    ofs.open("output.txt");
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < i + 1; j++)
-            ofs << num[i][j] << "\t";
-        ofs << endl;
-    }
+    ofs.close();
 }
